0329163acecf0447dabbd98fc615e86dc96fe7e6
y-du <42994541+y-du@users.noreply.github.com>
2026-09-01 14:59:22 +0200

    init

---
+++ pkg/components/new_impl/components/helper/configs/configs.go
@@ -0,0 +1,168 @@
+package configs
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"mgw-module-manager-migration/pkg/components/new_impl/models"
+	"mgw-module-manager-migration/pkg/components/new_impl/models/constants"
+	"mgw-module-manager-migration/pkg/components/old_impl/libs/module_lib"
+)
+
+func GetValue(val any, dataType int, isSlice bool) (models.Value, error) {
+	config := models.Value{
+		DataType: dataType,
+		IsSlice:  isSlice,
+	}
+	if isSlice {
+		anySlice, ok := val.([]any)
+		if !ok {
+			return models.Value{}, errors.New(fmt.Sprintf("invalid data type: '%T'", val))
+		}
+		switch dataType {
+		case constants.ValueDataTypeString:
+			for _, item := range anySlice {
+				v, err := toString(item)
+				if err != nil {
+					return models.Value{}, err
+				}
+				config.StringSlice = append(config.StringSlice, v)
+			}
+		case constants.ValueDataTypeBool:
+			for _, item := range anySlice {
+				v, err := toBool(item)
+				if err != nil {
+					return models.Value{}, err
+				}
+				config.BoolSlice = append(config.BoolSlice, v)
+			}
+		case constants.ValueDataTypeInt64:
+			for _, item := range anySlice {
+				v, err := toInt64(item)
+				if err != nil {
+					return models.Value{}, err
+				}
+				config.Int64Slice = append(config.Int64Slice, v)
+			}
+		case constants.ValueDataTypeFloat64:
+			for _, item := range anySlice {
+				v, err := toFloat64(item)
+				if err != nil {
+					return models.Value{}, err
+				}
+				config.Float64Slice = append(config.Float64Slice, v)
+			}
+		default:
+			return models.Value{}, errors.New(fmt.Sprintf("unsuported data type: '%d'", dataType))
+		}
+	} else {
+		switch dataType {
+		case constants.ValueDataTypeString:
+			v, err := toString(val)
+			if err != nil {
+				return models.Value{}, err
+			}
+			config.String = v
+		case constants.ValueDataTypeBool:
+			v, err := toBool(val)
+			if err != nil {
+				return models.Value{}, err
+			}
+			config.Bool = v
+		case constants.ValueDataTypeInt64:
+			v, err := toInt64(val)
+			if err != nil {
+				return models.Value{}, err
+			}
+			config.Int64 = v
+		case constants.ValueDataTypeFloat64:
+			v, err := toFloat64(val)
+			if err != nil {
+				return models.Value{}, err
+			}
+			config.Float64 = v
+		default:
+			return models.Value{}, errors.New(fmt.Sprintf("unsuported data type: '%d'", dataType))
+		}
+	}
+	return config, nil
+}
+
+func GetDataType(moduleDataType string) int {
+	return moduleDataTypeMap[moduleDataType]
+}
+
+func toString(val any) (string, error) {
+	v, ok := val.(string)
+	if !ok {
+		return "", errors.New("invalid data type: 'string' required")
+	}
+	return v, nil
+}
+
+func toBool(val any) (bool, error) {
+	v, ok := val.(bool)
+	if !ok {
+		return false, errors.New("invalid data type: 'boolean' required")
+	}
+	return v, nil
+}
+
+func float64ToInt64(val float64) (int64, bool) {
+	i, fr := math.Modf(val)
+	if fr > 0 {
+		return 0, false
+	}
+	return int64(i), true
+}
+
+func toInt64(val any) (int64, error) {
+	var i int64
+	switch v := val.(type) {
+	case int:
+		i = int64(v)
+	case int8:
+		i = int64(v)
+	case int16:
+		i = int64(v)
+	case int32:
+		i = int64(v)
+	case int64:
+		i = v
+	case float32:
+		var ok bool
+		i, ok = float64ToInt64(float64(v))
+		if !ok {
+			return 0, errors.New("invalid data type: 'integer' required")
+		}
+	case float64:
+		var ok bool
+		i, ok = float64ToInt64(v)
+		if !ok {
+			return 0, errors.New("invalid data type: 'integer' required")
+		}
+	default:
+		return 0, errors.New("invalid data type: 'integer' required")
+	}
+	return i, nil
+}
+
+func toFloat64(val any) (float64, error) {
+	var f float64
+	switch v := val.(type) {
+	case float32:
+		f = float64(v)
+	case float64:
+		f = v
+	default:
+		return 0, errors.New("invalid data type: 'float' required")
+	}
+	return f, nil
+}
+
+var moduleDataTypeMap = map[string]int{
+	module_lib.StringType:  constants.ValueDataTypeString,
+	module_lib.Int64Type:   constants.ValueDataTypeInt64,
+	module_lib.Float64Type: constants.ValueDataTypeFloat64,
+	module_lib.BoolType:    constants.ValueDataTypeBool,
+}
